bbf7f66c839aff4ab7925c3098d140f7f897b267
Nicolás Foti <nicolasdfoti@gmail.com>
2025-07-02 13:32:05 -0300

    Initial checkin on contacts

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/", require("./routes"));
+
+app.listen(PORT, () => {
+    console.log("running on port 3000");
+})
