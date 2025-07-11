2af704a658ed79958c8916c30d79d58bb91de57f
Nicolás Foti <nicolasdfoti@gmail.com>
2025-07-10 22:38:59 -0300

    Added API methods and Swagger

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -20,4 +20,50 @@ const getSingle = async (req, res) => {
     })
 };
 
-module.exports = {getAll, getSingle};
+const createContact = async (req, res) => {
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+    const response = await mongodb.getDb().db().collection("contacts").insertOne(contact);
+
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "An error ocurred");
+    }
+}
+
+const updateContact = async (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+    const response = await mongodb.getDb().db().collection("contacts").replaceOne({_id: contactId}, contact);
+
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "An error ocurred");
+    }
+}
+
+const deleteContact = async (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection("contacts").deleteOne({_id: contactId});
+
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "An error ocurred");
+    }
+}
+
+module.exports = {getAll, getSingle, createContact, updateContact, deleteContact};
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongodb = require("./data/database");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger.json");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+})
+
 // routes
 const routes = require("./routes");
 app.use("/", routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // database
 mongodb.initDb((err) => {
